04e5cc8ff0a0f49fec33740b1f0f1e7e6d38406d
Sai Nikhil Reddy Vatti <sainikhilreddyvatti@Nikhils-MacBook-Pro.local>
2025-01-25 00:13:06 -0600

    Added Tailwind css

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -5,7 +5,16 @@ namespace HMS_Dotnet.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        private string _password = string.Empty;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                // Hash the password
+                _password = Helpers.PasswordHelper.HashPassword(value);
+            }
+        }
         public string Phone { get; set; }
         public string Address { get; set; }
     }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Views/**/*.cshtml',  // Include Razor views
+    './wwwroot/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
